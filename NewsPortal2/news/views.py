cefd692d302f744ebe6ffb1003ce25a3e0c1c5d1
Дмитрий Михеев <143932457+miheich1911@users.noreply.github.com>
2024-02-09 15:18:27 +0300

    Delete NewsPortal2 directory

--- NewsPortal2/news/views.py
+++
@@ -1,8 +0,0 @@
-from django.views.generic import ListView
-from .models import *
-
-
-class NewsList(ListView):
-    model = Post
-    template_name = 'news/news_list.html'
-    context_object_name = 'news_list'
